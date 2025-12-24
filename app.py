fe1707c7ed10e7824bfb2b50fc0e024f9e01dac9
gaikwadsagar9 <cyperts.sagar@gmail.com>
2025-12-24 15:31:27 +0530

    updated status fun

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def home():
 def welcome():
     return "Welcome to the test data page!"
 
+@app.route("/testdata/status")
+def status():
+    return "Status: All systems operational."
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
