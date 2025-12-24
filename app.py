7c25eb6bb94d48eb4464c379bc2c0fd8ec6772d9
gaikwadsagar9 <cyperts.sagar@gmail.com>
2025-12-24 16:21:39 +0530

    update code 22113344

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def welcome():
     return "Welcome to the test data page!"
 
 @app.route("/testdata/status")
-def status():
+def status1():
     return "Status: All systems operational."
 
 if __name__ == "__main__":
