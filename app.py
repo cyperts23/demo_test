f76d8b756383ce235501ccc6332908f10d891900
gaikwadsagar9 <cyperts.sagar@gmail.com>
2025-12-24 15:24:38 +0530

    updated welcome fun

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route("/testdata")
 def home():
     return "🚀 Python App Deployed Successfully!"
+@app.route("/testdata/welcome")
+def welcome():
+    return "Welcome to the test data page!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
